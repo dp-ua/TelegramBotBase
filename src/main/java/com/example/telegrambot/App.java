5c36b00889c83e867926e86c03eccfc7a150ce15
Pavlo Reshetylo <pavlo_reshetylo@epam.com>
2019-12-04 14:51:36 +0200

    new token

--- src/main/java/com/example/telegrambot/App.java
+++ src/main/java/com/example/telegrambot/App.java
@@ -4,15 +4,12 @@ import com.example.telegrambot.bot.Bot;
 import org.apache.log4j.Logger;
 import org.telegram.telegrambots.ApiContextInitializer;
 
-/**
- * Hello world!
- */
 public class App {
     private static final Logger log = Logger.getLogger(App.class);
 
     public static void main(String[] args) {
         ApiContextInitializer.init();
-        Bot test_habr_bot = new Bot("test_habr_bot", "1012522162:AAHLvPVqKF48LdqnsvS3l5YrJfvFey6dBa0");
+        Bot test_habr_bot = new Bot("test_habr_bot", "1012522162:AAGfmwth5jQeZhLwiVEGs6bWbLHUTKuustE");
         test_habr_bot.botConnect();
     }
 }
