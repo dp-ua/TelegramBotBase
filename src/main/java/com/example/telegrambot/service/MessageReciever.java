34bff70d6e5a6ef74a86877595a58942cbddec1e
Pavlo Reshetylo <pavlo_reshetylo@epam.com>
2019-12-20 12:21:21 +0200

    add MessageReceiver

---
+++ src/main/java/com/example/telegrambot/service/MessageReciever.java
@@ -0,0 +1,90 @@
+package com.example.telegrambot.service;
+
+import com.example.telegrambot.bot.Bot;
+import com.example.telegrambot.command.Command;
+import com.example.telegrambot.command.ParsedCommand;
+import com.example.telegrambot.command.Parser;
+import com.example.telegrambot.handler.AbstractHandler;
+import com.example.telegrambot.handler.DefaultHandler;
+import com.example.telegrambot.handler.NotifyHandler;
+import com.example.telegrambot.handler.SystemHandler;
+import org.apache.log4j.Logger;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Update;
+
+
+public class MessageReciever implements Runnable {
+    private static final Logger log = Logger.getLogger(MessageReciever.class);
+    private final int WAIT_FOR_NEW_MESSAGE_DELAY = 1000;
+    private Bot bot;
+    private Parser parser;
+
+    public MessageReciever(Bot bot) {
+        this.bot = bot;
+        parser = new Parser(bot.getBotName());
+    }
+
+    @Override
+    public void run() {
+        log.info("[STARTED] MsgReciever.  Bot class: " + bot);
+        while (true) {
+            for (Object object = bot.receiveQueue.poll(); object != null; object = bot.receiveQueue.poll()) {
+                log.debug("New object for analyze in queue " + object.toString());
+                analyze(object);
+            }
+            try {
+                Thread.sleep(WAIT_FOR_NEW_MESSAGE_DELAY);
+            } catch (InterruptedException e) {
+                log.error("Catch interrupt. Exit", e);
+                return;
+            }
+        }
+    }
+
+    private void analyze(Object object) {
+        if (object instanceof Update) {
+            Update update = (Update) object;
+            log.debug("Update recieved: " + update.toString());
+            analyzeForUpdateType(update);
+        } else log.warn("Cant operate type of object: " + object.toString());
+    }
+
+    private void analyzeForUpdateType(Update update) {
+        Long chatId = update.getMessage().getChatId();
+        String inputText = update.getMessage().getText();
+
+        ParsedCommand parsedCommand = parser.getParsedCommand(inputText);
+        AbstractHandler handlerForCommand = getHandlerForCommand(parsedCommand.getCommand());
+
+        String operationResult = handlerForCommand.operate(chatId.toString(), parsedCommand, update);
+
+        if ("".equals(operationResult)) {
+            SendMessage message = new SendMessage();
+            message.setChatId(chatId);
+            message.setText(operationResult);
+            bot.sendQueue.add(message);
+        }
+    }
+
+    private AbstractHandler getHandlerForCommand(Command command) {
+        if (command == null) {
+            log.warn("Null command accepted. This is not good scenario.");
+            return new DefaultHandler(bot);
+        }
+        switch (command) {
+            case START:
+            case HELP:
+            case ID:
+                SystemHandler systemHandler = new SystemHandler(bot);
+                log.info("Handler for command[" + command.toString() + "] is: " + systemHandler);
+                return systemHandler;
+            case NOTIFY:
+                NotifyHandler notifyHandler = new NotifyHandler(bot);
+                log.info("Handler for command[" + command.toString() + "] is: " + notifyHandler);
+                return notifyHandler;
+            default:
+                log.info("Handler for command[" + command.toString() + "] not Set. Return DefaultHandler");
+                return new DefaultHandler(bot);
+        }
+    }
+}
