f42a817fc3678304d2177be989e2fcaaf102a3c4
Pavlo Reshetylo <pavlo_reshetylo@epam.com>
2019-12-20 14:43:46 +0200

    add starts for Sender, Receiver and send start message to admin.

--- src/main/java/com/example/telegrambot/App.java
+++ src/main/java/com/example/telegrambot/App.java
@@ -1,15 +1,46 @@
 package com.example.telegrambot;
 
 import com.example.telegrambot.bot.Bot;
+import com.example.telegrambot.service.MessageReciever;
+import com.example.telegrambot.service.MessageSender;
 import org.apache.log4j.Logger;
 import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
 
 public class App {
     private static final Logger log = Logger.getLogger(App.class);
+    private static final int PRIORITY_FOR_SENDER = 1;
+    private static final int PRIORITY_FOR_RECEIVER = 3;
+    private static final String BOT_ADMIN = "321644283";
 
     public static void main(String[] args) {
         ApiContextInitializer.init();
         Bot test_habr_bot = new Bot("test_habr_bot", "1012522162:AAGfmwth5jQeZhLwiVEGs6bWbLHUTKuustE");
+
+        MessageReciever messageReciever = new MessageReciever(test_habr_bot);
+        MessageSender messageSender = new MessageSender(test_habr_bot);
+
         test_habr_bot.botConnect();
+
+        Thread receiver = new Thread(messageReciever);
+        receiver.setDaemon(true);
+        receiver.setName("MsgReciever");
+        receiver.setPriority(PRIORITY_FOR_RECEIVER);
+        receiver.start();
+
+        Thread sender = new Thread(messageSender);
+        sender.setDaemon(true);
+        sender.setName("MsgSender");
+        sender.setPriority(PRIORITY_FOR_SENDER);
+        sender.start();
+
+        sendStartReport(test_habr_bot);
+    }
+
+    private static void sendStartReport(Bot bot) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(BOT_ADMIN);
+        sendMessage.setText("Запустился");
+        bot.sendQueue.add(sendMessage);
     }
 }
