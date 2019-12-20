5558fdb7fb37dfa8ea18cf0d14ee5638655354d9
Pavlo Reshetylo <pavlo_reshetylo@epam.com>
2019-12-20 14:37:05 +0200

    add System Handler

---
+++ src/main/java/com/example/telegrambot/handler/SystemHandler.java
@@ -0,0 +1,62 @@
+package com.example.telegrambot.handler;
+
+import com.example.telegrambot.bot.Bot;
+import com.example.telegrambot.command.Command;
+import com.example.telegrambot.command.ParsedCommand;
+import org.apache.log4j.Logger;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Update;
+
+public class SystemHandler extends AbstractHandler {
+    private static final Logger log = Logger.getLogger(SystemHandler.class);
+    private final String END_LINE = "\n";
+
+    public SystemHandler(Bot bot) {
+        super(bot);
+    }
+
+    @Override
+    public String operate(String chatId, ParsedCommand parsedCommand, Update update) {
+        Command command = parsedCommand.getCommand();
+
+        switch (command) {
+            case START:
+                bot.sendQueue.add(getMessageStart(chatId));
+                break;
+            case HELP:
+                bot.sendQueue.add(getMessageHelp(chatId));
+                break;
+            case ID:
+                return "Your telegramID: " + update.getMessage().getFrom().getId();
+        }
+        return "";
+    }
+
+    private SendMessage getMessageHelp(String chatID) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(chatID);
+        sendMessage.enableMarkdown(true);
+
+        StringBuilder text = new StringBuilder();
+        text.append("*This is help message*").append(END_LINE).append(END_LINE);
+        text.append("[/start](/start) - show start message").append(END_LINE);
+        text.append("[/help](/help) - show help message").append(END_LINE);
+        text.append("[/id](/id) - know your ID in telegram ").append(END_LINE);
+        text.append("/*notify* _time-in-sec_  - receive notification from me after the specified time").append(END_LINE);
+
+        sendMessage.setText(text.toString());
+        return sendMessage;
+    }
+
+    private SendMessage getMessageStart(String chatID) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(chatID);
+        sendMessage.enableMarkdown(true);
+        StringBuilder text = new StringBuilder();
+        text.append("Hello. I'm  *").append(bot.getBotName()).append("*").append(END_LINE);
+        text.append("I created specifically for resource habr.ru").append(END_LINE);
+        text.append("All that I can do - you can see calling the command [/help](/help)");
+        sendMessage.setText(text.toString());
+        return sendMessage;
+    }
+}
