01da6a3649d77328394379183be355ec28ef1bf0
Pavlo Reshetylo <pavlo_reshetylo@epam.com>
2019-12-23 10:57:33 +0200

    change token

--- src/main/java/com/example/telegrambot/App.java
+++ src/main/java/com/example/telegrambot/App.java
@@ -15,7 +15,7 @@ public class App {
 
     public static void main(String[] args) {
         ApiContextInitializer.init();
-        Bot test_habr_bot = new Bot("test_habr_bot", "1012522162:AAGfmwth5jQeZhLwiVEGs6bWbLHUTKuustE");
+        Bot test_habr_bot = new Bot("test_habr_bot", "1012522162:AAFZrajCVyKvPIQgTUMilj8l1P3ucjSAw8w");
 
         MessageReciever messageReciever = new MessageReciever(test_habr_bot);
         MessageSender messageSender = new MessageSender(test_habr_bot);
